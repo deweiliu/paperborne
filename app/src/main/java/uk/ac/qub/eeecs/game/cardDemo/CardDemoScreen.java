d1428502b2e668130247f3dba2cdcc26b4ffdc72
Cameron Stevenson <cwillis05@qub.ac.uk>
2018-02-14 18:10:49 +0000

    Merge branch 'master' of gitlab.eeecs.qub.ac.uk:csc-lvl2-1718/csc-lvl2-1718-115
    # Conflicts:
    #	app/src/test/java/uk/ac/qub/eeecs/gage/CardTests.java

--- app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/CardDemoScreen.java
+++ app/src/main/java/uk/ac/qub/eeecs/game/cardDemo/CardDemoScreen.java
@@ -3,15 +3,11 @@ package uk.ac.qub.eeecs.game.cardDemo;
 import android.graphics.Color;
 import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
 
 import uk.ac.qub.eeecs.gage.Game;
 import uk.ac.qub.eeecs.gage.engine.AssetStore;
 import uk.ac.qub.eeecs.gage.engine.ElapsedTime;
 import uk.ac.qub.eeecs.gage.engine.graphics.IGraphics2D;
-import uk.ac.qub.eeecs.gage.engine.input.Input;
-import uk.ac.qub.eeecs.gage.engine.input.TouchEvent;
 import uk.ac.qub.eeecs.gage.util.GraphicsHelper;
 import uk.ac.qub.eeecs.gage.util.Vector2;
 import uk.ac.qub.eeecs.gage.world.GameObject;
@@ -19,7 +15,6 @@ import uk.ac.qub.eeecs.gage.world.GameScreen;
 import uk.ac.qub.eeecs.gage.world.LayerViewport;
 import uk.ac.qub.eeecs.gage.world.ScreenViewport;
 import uk.ac.qub.eeecs.game.cardDemo.Cards.Card;
-import uk.ac.qub.eeecs.gage.ai.SteeringBehaviours;
 
 
 /**
